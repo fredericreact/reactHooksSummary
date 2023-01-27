5fadac6b99fd04992ea639b5d306e7fddfa998e9
unknown <fredericreact@gmail.com>
2023-01-27 20:23:46 +0800

    Getting Started with Custom Hooks

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -4,6 +4,7 @@ import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from './IngredientList'
 import ErrorModal from '../UI/ErrorModal'
+import useHttp from '../../hooks/http'
 
 const ingredientReducer = (currentIngredients, action) => {
 switch (action.type) {
@@ -19,24 +20,10 @@ switch (action.type) {
 }
 }
 
-const httpReducer = (curhttpState, action) => {
-  switch (action.type) {
-    case 'SEND':
-      return {loading: true, error: null};
-    case 'RESPONSE' : 
-    return {...curhttpState, loading:false}
-    case 'ERROR': 
-    return {loading:false, error:action.errorMessage}
-    case 'CLEAR':
-      return {...curhttpState, error: null}
-    default:
-      throw new Error('Should not be reached')
-  }
-}
 
 const Ingredients = () => {
   const [userIngredients, dispatch] = useReducer(ingredientReducer,[])
-  const [httpState, dispatchHttp] = useReducer(httpReducer, {loading: false, error:null})
+ const {isLoading, error, data, sendRequest}=useHttp()
   // const [userIngredients, setUserIngredients] = useState([])
   // const [isLoading, setIsLoading] = useState(false)
   // const [error, setError] = useState()
@@ -47,47 +34,37 @@ const Ingredients = () => {
   },[])
 
   const addIngredientHandler = useCallback(ingredient => {
-    dispatchHttp({type: 'SEND'})
-    fetch('https://react-http-26861-default-rtdb.firebaseio.com/ingredients.json', {
-      method: 'POST',
-      body: JSON.stringify(ingredient),
-      headers: {'Content-Type': 'application/json'}
+    // dispatchHttp({type: 'SEND'})
+    // fetch('https://react-http-26861-default-rtdb.firebaseio.com/ingredients.json', {
+    //   method: 'POST',
+    //   body: JSON.stringify(ingredient),
+    //   headers: {'Content-Type': 'application/json'}
 
-    }).then(response=> {
-      dispatchHttp({type:'RESPONSE'})
-      return response.json();
+    // }).then(response=> {
+    //   dispatchHttp({type:'RESPONSE'})
+    //   return response.json();
 
-    }).then(responseData => {
-      // setUserIngredients(prevIngredients => [
-      //   ...prevIngredients, 
-      //   {id: responseData.name, ...ingredient}
-      // ])  
-      dispatch({type: 'ADD', ingredient:{id: responseData.name, ...ingredient}})
+    // }).then(responseData => {
+    //   // setUserIngredients(prevIngredients => [
+    //   //   ...prevIngredients, 
+    //   //   {id: responseData.name, ...ingredient}
+    //   // ])  
+    //   dispatch({type: 'ADD', ingredient:{id: responseData.name, ...ingredient}})
       
-    })
+    // })
     
   },[]);
 
   const removeIngredientHandler = useCallback((ingredientId) => {
-    dispatchHttp({type:'SEND'})
 
-    fetch(`https://react-http-26861-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`, {
-      method: 'DELETE',
-    }).then(response => {
-      dispatchHttp({type:'REPONSE'})
-      // setUserIngredients(prevIngredients => {
-      //   return prevIngredients.filter(ingredient => ingredient.id!==ingredientId)
-      // })
-      dispatch({type: 'DELETE', id: ingredientId})
-    }).catch(error=>{
-      dispatchHttp({type: 'ERROR', errorMessage:'Something went wrong'})
-    })
-
-    
-  },[])
+sendRequest(
+  `https://react-http-26861-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`, 
+  'DELETE'
+  )
+  },[sendRequest])
 
   const clearError = useCallback(() => {
-    dispatchHttp({type:'CLEAR'})
+    // dispatchHttp({type:'CLEAR'})
 
   },[])
 
@@ -98,10 +75,10 @@ const Ingredients = () => {
 
   return (
     <div className="App">
-    {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
+    {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
       <IngredientForm 
       onAddIngredient={addIngredientHandler}
-      loading={httpState.loading}
+      loading={isLoading}
       />
 
       <section>
